0f90991fad140e802aa1d6ce24bccf9da8c43e9f
Adaxhy <147033905+Adaxhy@users.noreply.github.com>
2026-03-09 06:25:05 +0000

    Deploying to gh-pages from @ Adaxhy/Adaxhy.github.io@85cd3f7defce195e509c572142542763b4796c61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is the personal CV of me. Please check the official CV from pdf on the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
